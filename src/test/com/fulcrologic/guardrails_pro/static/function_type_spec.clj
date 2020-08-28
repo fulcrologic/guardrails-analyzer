2b6c37113f7423d2d8537d34486f924917d30612
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-08-27 20:46:28 -0700

    playing with custom testing assertions helper

--- src/test/com/fulcrologic/guardrails_pro/static/function_type_spec.clj
+++ src/test/com/fulcrologic/guardrails_pro/static/function_type_spec.clj
@@ -2,6 +2,7 @@
   (:require
     [clojure.spec.alpha :as s]
     [clojure.spec.gen.alpha :as gen]
+    [clojure.test :as t]
     [com.fulcrologic.guardrails-pro.runtime.artifacts :as grp.art]
     [com.fulcrologic.guardrails-pro.static.function-type :as grp.fnt]
     [com.fulcrologic.guardrails.core :as gr :refer [=> | <-]]
@@ -118,3 +119,85 @@
             (::grp.art/original-expression error) => 'x
             (::grp.art/actual error) => {::grp.art/failing-samples ["3"]}
             (-> error ::grp.art/expected ::grp.art/arg-types) => ["int?"]))))))
+
+
+
+
+
+(defn check-error? [x] (map? x))
+
+(defn all* [& checkers]
+  (fn [actual]
+    (filter check-error?
+        (flatten
+          (map #(% actual) checkers)))))
+
+(defn is?* [predicate]
+  (fn [actual]
+    (when-not (predicate actual)
+      {:actual actual
+       :expected predicate})))
+
+(defn equals?* [expected]
+  (fn [actual]
+    (when-not (= expected actual)
+      {:actual actual
+       :expected expected})))
+
+(defn valid?* [spec]
+  (fn [actual]
+    (when-not (s/valid? spec actual)
+      {:message (s/explain-str spec actual)
+       :actual actual
+       :expected spec})))
+
+(defn embeds?*
+  ([expected] (embeds?* expected []))
+  ([expected path]
+   (fn [actual]
+     (seq
+       (for [[k v] expected]
+         (let [actual-value (get actual k)
+               path (conj path k)]
+           (cond
+             (map? v) #_=> ((embeds?* v path) actual-value)
+             (not= actual-value v)
+             #_=> {:actual actual-value
+                   :expected v
+                   :message (str "at path " path ":")})))))))
+
+(defn check! [& checkers]
+  (fn [actual]
+    (when-let [errors ((apply all* checkers) actual)]
+      (doseq [e errors]
+          (t/do-report
+            (assoc e :type :fail))))
+    ;; silence =fn=> reporting
+    true))
+
+(specification "check!"
+  (let [x-double? (fn [actual]
+                    (when-not (double? (get-in actual [:x]))
+                      {:actual actual
+                       :expected `double?
+                       :message "x was not a double"}))
+        failing-checker (fn [actual]
+                          (vector
+                            (let [v (get-in actual [:FAKE/int])]
+                              (when-not (int? v)
+                                {:actual v, :expected `int?
+                                 :message ":FAKE/int was not an int"}))
+                            (let [v (get-in actual [:FAKE/string])]
+                              (when-not (string? v)
+                                {:actual v, :expected `string?
+                                 :message ":FAKE/string was not an string"}))))
+        data {:a 1 :b {:c 2 :d 3}}]
+    (assertions
+      1 => 1
+      ;123  =fn=> (check! (equals?* 456))
+      ;222  =fn=> (check! (is?* odd?))
+      ;{}   =fn=> (check! (valid?* ::grp.art/env))
+      ;data =fn=> (check! x-double?)
+      ;data =fn=> (check! failing-checker)
+      ;data =fn=> (check! (embeds?* {:a "A"}) (embeds?* {:b {:c "C"}}))
+      )))
