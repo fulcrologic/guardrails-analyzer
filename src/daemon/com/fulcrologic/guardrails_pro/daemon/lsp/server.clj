ebcda8a301b2cb402253b334ee6a4ef3d4c224a2
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-09-21 13:58:06 -0700

    lsp server can now handle reconnection and multiple clients

--- src/daemon/com/fulcrologic/guardrails_pro/daemon/lsp/server.clj
+++ src/daemon/com/fulcrologic/guardrails_pro/daemon/lsp/server.clj
@@ -1,5 +1,6 @@
 (ns com.fulcrologic.guardrails-pro.daemon.lsp.server
   (:require
+    [clojure.core.async :as async]
     [clojure.java.io :as io]
     [com.fulcrologic.guardrails-pro.artifacts :as grp.art]
     [taoensso.timbre :as log])
@@ -23,10 +24,11 @@
       TextDocumentSyncOptions)
     (org.eclipse.lsp4j.launch LSPLauncher)
     (org.eclipse.lsp4j.services LanguageServer TextDocumentService WorkspaceService)
-    (java.net ServerSocket)
+    (java.net ServerSocket SocketException SocketTimeoutException)
+    (java.util UUID)
     (java.util.concurrent CompletableFuture)))
 
-(defonce client (atom nil))
+(defonce clients (atom {}))
 
 (deftype LSPWorkspaceService []
   WorkspaceService
@@ -54,10 +56,11 @@
       DiagnosticSeverity/Error)
     "guardrails-pro"))
 
-(defn publish-diagnostics [client uri problems]
-  (.publishDiagnostics client
-    (new PublishDiagnosticsParams uri
-      (mapv problem->diagnostic problems))))
+(defn publish-diagnostics [uri problems]
+  (doseq [[_ client] @clients]
+    (.publishDiagnostics client
+      (new PublishDiagnosticsParams uri
+        (mapv problem->diagnostic problems)))))
 
 (deftype LSPTextDocumentService []
   TextDocumentService
@@ -65,22 +68,31 @@
     (let [document (.getTextDocument params)
           uri (.getUri document)]
       (log/info "didOpen:" uri)
-      (when true
-        (publish-diagnostics @client uri
-          [#::grp.art{:problem-type :error/foobar
-                      :message "AN ERROR mSG"
-                      :line-start 1 :line-end 1
-                      :column-start 0 :column-end 10}])))
+      (publish-diagnostics uri
+        [#::grp.art{:problem-type :error/foobar
+                    :message "AN ERROR mSG"
+                    :line-start 1 :line-end 1
+                    :column-start 0 :column-end 10}]))
     nil)
   (^void didChange [_ ^DidChangeTextDocumentParams params]
     (let [document (.getTextDocument params)
           uri (.getUri document)]
-      (log/info "didChange:" uri))
+      (log/info "didChange:" uri)
+      (publish-diagnostics uri
+        [#::grp.art{:problem-type :error/foobar
+                    :message "AN ERROR mSG"
+                    :line-start 1 :line-end 1
+                    :column-start 0 :column-end 10}]) )
     nil)
   (^void didSave [_ ^DidSaveTextDocumentParams params]
     (let [document (.getTextDocument params)
           uri (.getUri document)]
-      (log/info "didSave:" uri))
+      (log/info "didSave:" uri)
+      (publish-diagnostics uri
+        [#::grp.art{:problem-type :error/foobar
+                    :message "AN ERROR mSG"
+                    :line-start 1 :line-end 1
+                    :column-start 0 :column-end 10}]))
     nil)
   (^void didClose [_ ^DidCloseTextDocumentParams params]
     (let [document (.getTextDocument params)
@@ -88,27 +100,33 @@
       (log/info "didClose:" uri))
     nil))
 
-(def server
-  (proxy [LanguageServer] []
-    (^CompletableFuture initialize [^InitializeParams params]
-      (log/info "initialize!")
-      (CompletableFuture/completedFuture
-        (new InitializeResult
-          (doto (new ServerCapabilities)
-            (.setTextDocumentSync
-              (doto (new TextDocumentSyncOptions)
-                (.setOpenClose true)))))))
-    (^void initialized [^InitializedParams params]
-      (log/info "initialized!"))
-    (^CompletableFuture shutdown []
-      (log/info "shutdown!")
-      (CompletableFuture/completedFuture 0))
-    (^void exit []
-      (log/info "exit!"))
-    (getTextDocumentService []
-      (new LSPTextDocumentService))
-    (getWorkspaceService []
-      (new LSPWorkspaceService))))
+(defn new-server [launcher]
+  (let [client-id (atom nil)]
+    (proxy [LanguageServer] []
+      (^CompletableFuture initialize [^InitializeParams params]
+        (let [id (UUID/randomUUID)]
+          (log/info "initialize server!" {:id id})
+          (swap! clients assoc id (.getRemoteProxy @launcher))
+          (reset! client-id id))
+        (CompletableFuture/completedFuture
+          (new InitializeResult
+            (doto (new ServerCapabilities)
+              (.setTextDocumentSync
+                (doto (new TextDocumentSyncOptions)
+                  (.setOpenClose true)))))))
+      (^void initialized [^InitializedParams params]
+        (log/info "client initialized!"))
+      (^CompletableFuture shutdown []
+        (log/info "shutdown!" @client-id)
+        (swap! clients dissoc @client-id)
+        (reset! client-id nil)
+        (CompletableFuture/completedFuture 0))
+      (^void exit []
+        (log/info "exit!"))
+      (getTextDocumentService []
+        (new LSPTextDocumentService))
+      (getWorkspaceService []
+        (new LSPWorkspaceService)))))
 
 (defn find-port-file
   [^java.io.File start-dir]
@@ -125,31 +143,56 @@
   (io/make-parents file)
   (spit file port))
 
+(defn- shutdown-socket [s]
+  (when-not (.isInputShutdown s)
+    (.shutdownInput s))
+  (when-not (.isOutputShutdown s)
+    (.shutdownOutput s)))
+
 (defn start-lsp []
   (log/info "Starting guardrails LSP server!")
-  (let [server-socket (new ServerSocket 0)
+  (let [server-socket (doto (new ServerSocket 0)
+                        (.setSoTimeout 1000))
         port-file (find-port-file ".")]
     (.deleteOnExit port-file)
     (write-port-to-file! port-file
       (.getLocalPort server-socket))
-    (let [socket (.accept server-socket)
-          launcher (LSPLauncher/createServerLauncher server
-                     (.getInputStream socket)
-                     (.getOutputStream socket))]
-      (reset! client (.getRemoteProxy launcher))
-      (.startListening launcher)
-      {::client client
-       ::port-file port-file
-       ::server server
-       ::server-socket server-socket
-       ::socket socket})))
+    (let [sockets (atom [])
+          stop-chan (async/chan)]
+      (async/go-loop []
+        (if (= ::STOP (async/alt!
+                        stop-chan ([v] v)
+                        (async/timeout 1000) nil))
+          (do (log/info "Received stop signal, shutting down guardrails LSP server!")
+            (.close server-socket)
+            (reset! clients {})
+            (doseq [s @sockets]
+              (shutdown-socket s))
+            (reset! sockets []))
+          (do (when-let [socket (try (.accept server-socket)
+                                  (catch SocketTimeoutException _ nil)
+                                  (catch SocketException e
+                                    (log/error e "socket exception")
+                                    nil))]
+                (let [launcher (atom nil)
+                      server (new-server launcher)]
+                  (reset! launcher
+                    (LSPLauncher/createServerLauncher server
+                      (.getInputStream socket)
+                      (.getOutputStream socket)))
+                  (swap! sockets conj socket)
+                  (async/go-loop [F (.startListening @launcher)]
+                    (if (future-done? F)
+                      (do (log/debug "future-done!")
+                        (shutdown-socket socket)
+                        @(.shutdown server)
+                        (.close socket))
+                      (recur F)))))
+            (recur))))
+      {::port-file port-file
+       ::stop-chan stop-chan})))
 
-(defn stop-lsp [{::keys [port-file server socket server-socket]} ]
+(defn stop-lsp [{::keys [port-file stop-chan]} ]
   (log/info "Stopping guardrails LSP server!")
   (.delete port-file)
-  (.shutdownInput socket)
-  (.shutdownOutput socket)
-  @(.shutdown server)
-  (.exit server)
-  (.close socket)
-  (.close server-socket))
+  (async/put! stop-chan ::STOP))
