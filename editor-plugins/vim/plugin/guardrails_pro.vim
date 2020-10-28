b44573545a4d7ac6d366f8c32ad649b149a472e5
Anthony D'Ambrosio <anthony.dayzerostudio@gmail.com>
2020-10-27 17:06:04 -0700

    adding check root form (from cursor) command (lsp & vim)

--- editor-plugins/vim/plugin/guardrails_pro.vim
+++ editor-plugins/vim/plugin/guardrails_pro.vim
@@ -1,5 +1,12 @@
-function! guardrails_pro#run_check()
+function! guardrails_pro#check_current_file()
   call ale#lsp_linter#SendRequest(bufnr('%'), "guardrails",
         \ [0, 'workspace/executeCommand'
-        \ , {'command': 'check!', 'arguments': expand('%:p')}])
+        \ , {'command': 'check-file!', 'arguments': expand('%:p')}])
+endfunction
+
+function! guardrails_pro#check_root_form()
+  call ale#lsp_linter#SendRequest(bufnr('%'), "guardrails",
+        \ [0, 'workspace/executeCommand'
+        \ , {'command'  : 'check-root-form!'
+        \   ,'arguments': [expand('%:p'), line('.')]}])
 endfunction
--- src/daemon/com/fulcrologic/guardrails_pro/daemon/lsp/commands.clj
+++ src/daemon/com/fulcrologic/guardrails_pro/daemon/lsp/commands.clj
@@ -5,14 +5,30 @@
     [com.fulcrologic.guardrails-pro.forms :as grp.forms]
     [taoensso.timbre :as log]))
 
-(defn check! [path]
-  (log/debug "commands/check!" path)
-  ;; NOTE: path currently always cljs
+(defn check-file! [path]
+  (log/debug "commands/check-file!" path)
   (let [{:keys [NS forms]} (reader/read-file path)]
     (notify-checkers! :check!
       {:forms (grp.forms/form-expression forms)
        :file  path
        :NS    NS})))
 
+(defn root-form-at? [cursor-line ?form]
+  (let [{:keys [line end-line]} (meta ?form)]
+    (<= line cursor-line end-line)))
+
+(defn check-root-form! [path line]
+  (log/debug "commands/check-root-form!" path line)
+  (let [{:keys [NS forms]} (reader/read-file path)]
+    (notify-checkers! :check!
+      {:forms (grp.forms/form-expression
+                (filter (partial root-form-at? line)
+                  forms))
+       :file path
+       :NS   NS})))
+
 (def commands
-  {"check!" check!})
+  {"check-file!" check-file!
+   "check-root-form!" check-root-form!
+   ;; TODO ? force check variants (no cache)
+   })
